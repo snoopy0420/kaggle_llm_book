06e02687112f607f19e71caaec32529b3d35acfd
runpod <runpod@example.com>
2026-03-15 08:34:28 +0000

    feat: ch3.5実装

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
-# プロジェクト開始時に使用する
+# storageが初期化された場合に使用する
 # ターミナルを再起動すると環境変数のこの変更が確認できる。
 # windowをリロードするとソース管理も反映される。
 # vscodeのフォルダもatma_udemyを開きカーネル(.venv/bin/python)を作成する。
@@ -18,31 +18,15 @@ mkdir -p /workspace/.cache/uv
 # gitのセットアップ
 git config --global user.email "runpod@example.com"
 git config --global user.name "runpod"
+uv run nbstripout --install
 
 # git clone
 cd /workspace 
-git clone {もとにするgitリポジトリurl} # 要修正
+git clone https://github.com/snoopy0420/atma_udemy.git # 要修正
 
-# gitプロジェクトの初期化
-# 事前に新しいプロジェクト用のgitリポジトリを作成しておく
-cd /workspace/{もとにするgitリポジトリのリポジトリ名}/ # 要修正
-rm -rf .git
-git init
-git add .
-git commit -m "Initial commit"
-git remote add origin {新しいgitリポジトリurl} # 要修正
-git branch -M main
-git push -u origin main
-
-# uvの初期化
-uv init
-uv add --dev nbstripout ipykernel
-uv python install 3.12.12 # 要修正
-
-# ipynbの出力をコミット時に自動削除する
-uv run nbstripout --install
-
-
+# パッケージのsync
+cd /workspace/atma_udemy/ # 要修正
+uv sync
 
 
 
