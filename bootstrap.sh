e6577a6ecd744642a462532a4d6b386bde362160
runpod <runpod@example.com>
2026-03-15 10:27:45 +0000

    feat: ch3.4_practice実装中

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,7 @@
 # podのリスタート時に実行する
-# ターミナルを再起動すると環境変数のこの変更が確認できる。
-# windowをリロードするとソース管理も反映される。
-# vscodeのフォルダもatma_udemyを開きカーネル(.venv/bin/python)を作成する。
 
 # bashスクリプトの安全装置3点セット
 set -euo pipefail
-
-# uvのセットアップ
-## インストール
-# curl -LsSf https://astral.sh/uv/install.sh | sh
-# ## パスを通す
-# export PATH=/root/.local/bin:$PATH
-# ## uvのキャッシュを/workspace配下にする
-# export UV_CACHE_DIR=/workspace/.cache/uv
-# mkdir -p /workspace/.cache/uv
-
-# パッケージのインストール
-# cd /workspace/atma_udemy/
-# uv sync
 
 # gitのセットアップ
 git config --global user.email "runpod@example.com"
